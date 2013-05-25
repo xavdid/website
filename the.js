4388802e719513af95b2307700f253459c5521fb
David Brownman <beamneocube@gmail.com>
2013-05-25 01:16:11 -0500

    javascript color changes? may be better, idk

--- the.js
+++ the.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('a').draggable();
+    $('img')
+        .mouseenter(function(){
+            var src = $(this).attr('src').replace(".png","1.png");
+            $(this).attr("src",src);
+        })
+        .mouseleave(function(){
+            var src2 = $(this).attr('src').replace("1.png",".png");
+            $(this).attr("src",src2);
+        });
 });
