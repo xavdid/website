ea6762caaed1858cef256e01b998ed11becda067
David Brownman <beamneocube@gmail.com>
2013-09-19 17:11:00 -0400

    added resume

--- the.js
+++ the.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
     $('a').draggable();
-    // $('img') //this is slower
-    //     .mouseenter(function(){
-    //         var src = $(this).attr('src').replace(".png","1.png");
-    //         $(this).attr("src",src);
-    //     })
-    //     .mouseleave(function(){
-    //         var src2 = $(this).attr('src').replace("1.png",".png");
-    //         $(this).attr("src",src2);
-    //     });
 });
