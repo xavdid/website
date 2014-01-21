82f866c1a205d68fe419c99141d380793965d3b8
David Brownman <beamneocube@gmail.com>
2014-01-21 14:06:24 -0500

    fixed some footer spacing

--- js/facts.js
+++ js/facts.js
@@ -11,7 +11,7 @@ var facts = [
     'I always set my alarms so their digit column is 3 or 7.',
     'XKCD is brilliant, especially <a href="http://xkcd.com/1037/" target="_blank">this</a> one.',
     'This site is written in <a href="http://haml.info">haml</a>, meaning I can\'t end a sentence in a link or it looks like <a href="#">this</a> . This lead to a lot of non-concise language to re-order word choice.',
-    'I\'ve played alto sax since 5th grade. In college, I played in the Pops Orchestra and still fiddle around when I get the chance',
+    'I\'ve played alto sax since 5th grade. In college, I played in the Pops Orchestra and still fiddle around when I get the chance.',
     'I run a <a href="http://www.twitter.com/not_andrew_ax">parody twitter account</a> about one of my housemates. It\'s actually pretty funny if you know him at all. Imagine a lax bro who picked up a CS degree. It mainly fun because I really get into his head when I tweet.',
     'Every Friday during senior year of high school, my friends and I dressed up in fancy suits. We called it "Fancy Friday" and got in the yearbook because of it. I still really enjoy dressing up in thrift store gear (which we did before it was cool).',
     '<a href="http://www.spacechemthegame.com"> Space Chem</a> may be the most challenging, rewarding, and well designed game (puzzle or otherwise) that I\'ve ever played. Whenever you feel too smart, it\'s there to take you down a peg or two, but in a good way.',
