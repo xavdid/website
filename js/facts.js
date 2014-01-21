de62300daf91d666c44c0bfb4ac258c652a3032f
David Brownman <beamneocube@gmail.com>
2014-01-21 00:38:02 -0500

    pushing jenny changes

--- js/facts.js
+++ js/facts.js
@@ -3,7 +3,7 @@ var facts = [
     'I stole the random fact idea idea from <a href="http://nicholasquinlan.com" target="_blank"> Nick Quinlan</a>, who is a wonderful human being.',
     'I really like design, but I\'m terrible at starting from scratch.',
     'If you reload this site enough, you\'ll eventually see all the facts.',
-    'I was born in Houston, TX, lived in Boulder, CO from 4th to 10th grade, then moved back to Houston for the last two years of high school. I never really know what to say when people ask where I\'m from.',
+    'I was born in Houston, TX, lived in Boulder, CO from 4th to 10th grade, moved back to Houston for the last two years of high school. I never really know what to say when people ask where I\'m from.',
     'I really like teaching. If software dev doesn\'t work out for whatever reason, I\'ll probably be a computer science professor.',
     'When I lived in Boulder, I biked a ton. I wish I did more now.',
     'I attended (and then worked at) <a href="http://www.campstewart.com" target="_blank">Camp Stewart</a> for 7 different summers. More than anything else, I believe that affected the person I became today. Everyone should go to camp.',
@@ -14,9 +14,10 @@ var facts = [
     'I\'ve played alto sax since 5th grade. In college, I played in the Pops Orchestra and still fiddle around when I get the chance',
     'I run a <a href="http://www.twitter.com/not_andrew_ax">parody twitter account</a> about one of my housemates. It\'s actually pretty funny if you know him at all. Imagine a lax bro who picked up a CS degree. It mainly fun because I really get into his head when I tweet.',
     'Every Friday during senior year of high school, my friends and I dressed up in fancy suits. We called it "Fancy Friday" and got in the yearbook because of it. I still really enjoy dressing up in thrift store gear (which we did before it was cool).',
-    '<a href="http://www.spacechemthegame.com"> Space Chem</a> may be the most challenging, rewarding, and well designed game I\'ve ever played. Whenever you feel too smart, it\'s there to take you down a peg or two, in a good way.',
-    'There are shockingly few other David Brownmans in the world. One of them is an expert on <a href="http://books.google.com/books?id=X8waCmzjiD4C&pg=PA996&lpg=PA996&dq=%22david+brownman%22&source=bl&ots=hG7slZ52O3&sig=AF6Yt-ri3v8wORJMl6UvMberDwY&hl=en&sa=X&ei=eY_XUtCRDtWksQT74ICgCw&ved=0CE8Q6AEwBTgU#v=onepage&q=%22david%20brownman%22&f=false">shamanism</a>.',
-    'My favorite subreddit is probably <a href="http://www.reddit.com/r/nottheonion">r/nottheonion</a>.'
+    '<a href="http://www.spacechemthegame.com"> Space Chem</a> may be the most challenging, rewarding, and well designed game (puzzle or otherwise) that I\'ve ever played. Whenever you feel too smart, it\'s there to take you down a peg or two, but in a good way.',
+    'There are shockingly few other David Brownmans in the world. One of them is an expert on <a href="http://books.google.com/books?id=X8waCmzjiD4C&pg=PA996&lpg=PA996&dq=%22david+brownman%22&source=bl&ots=hG7slZ52O3&sig=AF6Yt-ri3v8wORJMl6UvMberDwY&hl=en&sa=X&ei=eY_XUtCRDtWksQT74ICgCw&ved=0CE8Q6AEwBTgU#v=onepage&q=%22david%20brownman%22&f=false" target="_blank">shamanism</a>.',
+    'My favorite subreddit is probably <a href="http://www.reddit.com/r/nottheonion">r/nottheonion</a>.', 
+    'My car is a 2002 Honda Accord and it\'s got manual everything- locks, windows, etc. My grandfather got it that way because he firmly believed that anything electric was unreliable, so he sprung for the crank windows.'
 ];
 
 function getRandomInt (min, max) {
