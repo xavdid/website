ec7ecb210ef48b743f632462ef861448824e4ef0
David Brownman <beamneocube@gmail.com>
2015-03-16 15:51:42 -0700

    fixed typos and updated some facts

--- js/facts.js
+++ js/facts.js
@@ -12,12 +12,13 @@ var facts = [
     'XKCD is brilliant, especially <a href="http://xkcd.com/1037/" target="_blank">this</a> one.',
     'This site is written in <a href="http://haml.info">haml</a>, meaning I can\'t end a sentence in a link or it looks like <a href="#">this</a> . This lead to a lot of non-concise language to re-order word choice.',
     'I\'ve played alto sax since 5th grade. In college, I played in the Pops Orchestra and still fiddle around when I get the chance.',
-    'I run a <a href="http://www.twitter.com/not_andrew_ax">parody twitter account</a> about one of my housemates. It\'s actually pretty funny if you know him at all. Imagine a lax bro who picked up a CS degree. It mainly fun because I really get into his head when I tweet.',
     'Every Friday during senior year of high school, my friends and I dressed up in fancy suits. We called it "Fancy Friday" and got in the yearbook because of it. I still really enjoy dressing up in thrift store gear (which we did before it was cool).',
     '<a href="http://www.spacechemthegame.com"> Space Chem</a> may be the most challenging, rewarding, and well designed game (puzzle or otherwise) that I\'ve ever played. Whenever you feel too smart, it\'s there to take you down a peg or two, but in a good way.',
     'There are shockingly few other David Brownmans in the world. One of them is an expert on <a href="http://books.google.com/books?id=X8waCmzjiD4C&pg=PA996&lpg=PA996&dq=%22david+brownman%22&source=bl&ots=hG7slZ52O3&sig=AF6Yt-ri3v8wORJMl6UvMberDwY&hl=en&sa=X&ei=eY_XUtCRDtWksQT74ICgCw&ved=0CE8Q6AEwBTgU#v=onepage&q=%22david%20brownman%22&f=false" target="_blank">shamanism</a>.',
     'My favorite subreddit is probably <a href="http://www.reddit.com/r/nottheonion">r/nottheonion</a>.', 
-    'My car is a 2002 Honda Accord and it\'s got manual everything (besides transmission)- locks, windows, etc. My grandfather got it that way because he firmly believed that anything electric was unreliable, so he sprung for the crank windows.'
+    'My car is a 2002 Honda Accord and it\'s got manual everything (besides transmission)- locks, windows, etc. My grandfather got it that way because he firmly believed that anything electric was unreliable, so he sprung for the crank windows.',
+    'I live in the Silicon Valley Tech House, complete with computers on tables, bunk beds, and weeky GoT watching.',
+    
 ];
 
 function getRandomInt (min, max) {
