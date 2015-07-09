1603bcd97c46572d90100703370b6156cd9f4de4
David Brownman <beamneocube@gmail.com>
2015-07-08 23:26:38 -0700

    added some facts and updated projects

--- source/js/facts.js
+++ source/js/facts.js
@@ -1,10 +1,10 @@
 // If you're reading this, you're a cheater.
 var facts = [
     'I stole the random fact idea idea from <a href="http://nicholasquinlan.com" target="_blank"> Nick Quinlan</a>, who is a wonderful human being.',
-    'I really like design, but I\'m terrible at starting from scratch.',
+    'I really like design, but I\'m terrible at starting anything from scratch.',
     'If you reload this site enough, you\'ll eventually see all the facts.',
     'I was born in Houston, TX, lived in Boulder, CO from 4th to 10th grade, moved back to Houston for the last two years of high school. I never really know what to say when people ask where I\'m from.',
-    'I really like teaching. If software dev doesn\'t work out for whatever reason, I\'ll probably be a computer science professor.',
+    'I really like teaching. Maybe one day, I\'ll be a computer science professor.',
     'When I lived in Boulder, I biked a ton. I wish I did more now.',
     'I attended (and then worked at) <a href="http://www.campstewart.com" target="_blank">Camp Stewart</a> for 7 different summers. More than anything else, I believe that affected the person I became today. Everyone should go to camp.',
     'College and summer camp are basically the same experience. One has more beer.',
@@ -15,10 +15,14 @@ var facts = [
     'Every Friday during senior year of high school, my friends and I dressed up in fancy suits. We called it "Fancy Friday" and got in the yearbook because of it. I still really enjoy dressing up in thrift store gear (which we did before it was cool).',
     '<a href="http://www.spacechemthegame.com"> Space Chem</a> may be the most challenging, rewarding, and well designed game (puzzle or otherwise) that I\'ve ever played. Whenever you feel too smart, it\'s there to take you down a peg or two, but in a good way.',
     'There are shockingly few other David Brownmans in the world. One of them is an expert on <a href="http://books.google.com/books?id=X8waCmzjiD4C&pg=PA996&lpg=PA996&dq=%22david+brownman%22&source=bl&ots=hG7slZ52O3&sig=AF6Yt-ri3v8wORJMl6UvMberDwY&hl=en&sa=X&ei=eY_XUtCRDtWksQT74ICgCw&ved=0CE8Q6AEwBTgU#v=onepage&q=%22david%20brownman%22&f=false" target="_blank">shamanism</a>.',
-    'My favorite subreddit is probably <a href="http://www.reddit.com/r/nottheonion">r/nottheonion</a>.', 
     'My car is a 2002 Honda Accord and it\'s got manual everything (besides transmission)- locks, windows, etc. My grandfather got it that way because he firmly believed that anything electric was unreliable, so he sprung for the crank windows.',
-    'I live in the Silicon Valley Tech House, complete with computers on tables, bunk beds, and weeky GoT watching.',
-    'I\'ve been involved in the global <a href="https://en.wikipedia.org/wiki/Muggle_quidditch">quidditch</a> scene since 2010.'    
+    'I\'ve been involved in the global <a href="https://en.wikipedia.org/wiki/Muggle_quidditch">quidditch</a> scene since 2010.',
+    'My favorite subreddit is <a href="http://www.reddit.com/r/subredditsimulator">r/subredditsimulator</a>.', 
+    'My car is a 2002 Honda Accord and it\'s got manual everything (besides transmission)- locks, windows, etc. My grandfather got it that way because he firmly believed that anything electric was unreliable, so he sprung for the crank windows.',
+    'I sold a bunch of magic cards to a card shop in exchange for a gratuitous amount of store credit. So, we\'ve got a sweet collection of comics and board games going, basically for free.',
+    'Our apartment is basically an ikea showroom.',
+    'When I first moved to California, I lived in the "Silicon Valley Tech House" in full sized bunk beds with 11 other people. HBO\'s Silicon Valley was basically a documentary about our lives.',
+    'I\'ve gotten very good at taking 6am flights to locations on the east coast because that\'s best way to travel.'
 ];
 
 function getRandomInt (min, max) {
