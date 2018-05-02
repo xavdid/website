7355048912e2e308969aae29b87c0a791f07fba5
David Brownman <beamneocube@gmail.com>
2018-05-01 22:51:18 -0700

    Automated commit at 2018-05-02 05:51:18 UTC by middleman-deploy 2.0.0-alpha

---
+++ js/facts-f34b87aa.js
@@ -0,0 +1,41 @@
+/* globals $ */
+
+// If you're reading this, you're a cheater.
+var facts = [
+  'I stole the random fact idea idea from <a href="http://nicholasquinlan.com" target="_blank" rel="noopener"> Nick Quinlan</a>, who is a wonderful human being.',
+  "I really like design, but I'm terrible at starting anything from scratch.",
+  "If you reload this site enough, you'll eventually see all the facts.",
+  "I was born in Houston, TX, lived in Boulder, CO from 4th to 10th grade, moved back to Houston for the last two years of high school. I never really know what to say when people ask where I'm from.",
+  "I really like teaching. Maybe one day, I'll be a computer science professor.",
+  'When I lived in Boulder, I biked a ton. I wish I did more now.',
+  'I attended (and then worked at) <a href="http://www.campstewart.com" target="_blank" rel="noopener">Camp Stewart</a> for 7 different summers. More than anything else, I believe that affected the person I became today. Everyone should go to camp.',
+  'College and summer camp are basically the same experience. One has more beer.',
+  'I always set my alarms so their digit column is 3 or 7.',
+  'XKCD is brilliant, especially <a href="https://xkcd.com/1037/" target="_blank" rel="noopener">this</a> one.',
+  'This site is written in <a href="https://haml.info">haml</a>, meaning I can\'t natively end a sentence in a link or it looks like <a href="#">this</a> . This lead to a lot of non-concise language in the interest of using the language as it was meant to be used.',
+  "I've played alto sax since 5th grade. In college, I played in the Pops Orchestra and still fiddle around when I get the chance.",
+  'Every Friday during senior year of high school, my friends and I dressed up in fancy suits. We called it "Fancy Friday" and got in the yearbook because of it. I still really enjoy dressing up in thrift store gear (which we did before it was cool).',
+  "<a href=\"https://store.steampowered.com/app/210970/The_Witness/\"> The Witness</a> may be the most challenging, rewarding, and well designed game (puzzle or otherwise) that I've ever played. Whenever you feel too smart, it's there to take you down a peg or two, but in a good way.",
+  'There are shockingly few other David Brownmans in the world. One of them is an expert on <a href="https://books.google.com/books?id=X8waCmzjiD4C&pg=PA996&lpg=PA996&dq=%22david+brownman%22&source=bl&ots=hG7slZ52O3&sig=AF6Yt-ri3v8wORJMl6UvMberDwY&hl=en&sa=X&ei=eY_XUtCRDtWksQT74ICgCw&ved=0CE8Q6AEwBTgU#v=onepage&q=%22david%20brownman%22&f=false" target="_blank" rel="noopener">shamanism</a>.',
+  'I\'ve been involved in the global <a href="https://en.wikipedia.org/wiki/Muggle_quidditch">quidditch</a> scene since 2010.',
+  'My favorite subreddit is <a href="https://www.reddit.com/r/subredditsimulator">r/subredditsimulator</a>.',
+  "I sold a bunch of magic cards to a card shop in exchange for a gratuitous amount of store credit. So, we've got a sweet collection of comics and board games going, basically for free.",
+  // "All of the furniture in my apartment is Ikea. It's basically an showroom at this point.",
+  'When I first moved to California, I lived in the "Silicon Valley Tech House" in adult-size bunk beds with 11 other people. HBO\'s Silicon Valley was basically a documentary about our lives.',
+  "I've gotten very good at taking 6am flights to locations on the east coast because that's best way to travel eastward without losing your entire day.",
+  "I prefer Marvel to DC, though I've got a soft spot for Batman",
+  'I won an award at work once that came with a huge lego rocket ship!',
+  "Each blog post comes with a matching historical event that happened the same day. As a rule, I use the earliest semi-interesting fact I can find. The facts favor things Wikipedia decides are notable, since that's where I get my facts.",
+  // 'While funemployed, I worked through a <a href="https://publists.herokuapp.com/user/6452502/lists/250731358" target="_blank" rel="noopener">list</a> of tasks I hadn\'t previously had time for. I\'ve got a job now, but it\'s still a nice list of things to do when you\'ve got a lot of free time',
+  'My favorite food in the world is Mac & Cheese',
+  'I made a $20 bet to not purchase any new videogames in 2017 (and work on my Steam backlog instead). I won the bet!',
+  'I have lived at least 4 years in every major US timezone',
+  "I really like structured data, especially JSON. That's why my resume is JSON, as is my favorite <a href=\"https://jsonfeed.org/\">syndication format</a>"
+]
+
+function getRandomIndex (max) {
+  return Math.floor(Math.random() * (max - 0 + 1))
+}
+
+var i = getRandomIndex(facts.length - 1)
+$('.fact').html(facts[i])
