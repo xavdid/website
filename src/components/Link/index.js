6691f19b18d1e37b9491bfae561493dbe0699c45
David Brownman <beamneocube@gmail.com>
2022-02-07 21:59:17 -0800

    add analytics for outgoing links

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
+import { useGoatCounter } from "gatsby-plugin-goatcounter";
 
 import "./link.scss";
 
@@ -9,30 +10,39 @@ export default ({
   children,
   className = "body-link",
   ...props
-}) => (
-  <>
-    {href &&
-      (href.startsWith("/") ? (
-        // internal link
-        <Link className={className} to={href} {...props}>
-          {children}
-        </Link>
-      ) : href.startsWith("#") ? (
-        // local anchor
-        <a className={className} href={href}>
-          {children}
-        </a>
-      ) : (
-        // external link
-        <a
-          className={className}
-          target="_blank"
-          rel="noreferrer"
-          href={href}
-          data-goatcounter-click={`ext-${href}`}
-        >
-          {children}
-        </a>
-      ))}
-  </>
-);
+}) => {
+  const count = useGoatCounter();
+  return (
+    <>
+      {href &&
+        (href.startsWith("/") ? (
+          // internal link
+          <Link className={className} to={href} {...props}>
+            {children}
+          </Link>
+        ) : href.startsWith("#") ? (
+          // local anchor
+          <a className={className} href={href}>
+            {children}
+          </a>
+        ) : (
+          // external link
+          <a
+            className={className}
+            target="_blank"
+            rel="noreferrer"
+            href={href}
+            onClick={() => {
+              // log the event manually, but don't preventDefault()
+              count({
+                path: `ext-${href}`,
+                event: true,
+              });
+            }}
+          >
+            {children}
+          </a>
+        ))}
+    </>
+  );
+};
