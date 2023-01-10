bec948e01c513ea935b3b4dbc41b18034f470372
David Brownman <beamneocube@gmail.com>
2023-01-09 20:04:52 -0800

    fix unstyled icon flash

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,12 @@
 import littlefoot from "littlefoot";
 
+// need to import this css so it's included in the bundle and not loaded
+//   async later (causing an unstyled content flash)
+// https://github.com/FortAwesome/react-fontawesome/issues/234
+// https://dev.to/kayla/how-to-fix-font-awesome-icons-flashing-wrong-size-in-gatsby-3igm
+require("@fortawesome/fontawesome-svg-core/styles.css");
+require("@fortawesome/fontawesome-svg-core").autoAddCss = false;
+
 // these need to be `require`s, not es6 imports
 // see: https://github.com/goblindegook/littlefoot/issues/338
 require("littlefoot/dist/littlefoot.css");
