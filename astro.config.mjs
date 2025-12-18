779ae5eb43df6a2e25e469845cc7c7add16823b3
David Brownman <oss@xavd.id>
2025-12-17 21:16:23 -0800

    anchor hover works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,34 @@
 // @ts-check
 
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://xavd.id",
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [
+    mdx({
+      rehypePlugins: [
+        // the IDs get generated without this, but the autolinking doesn't work?
+        rehypeHeadingIds,
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: "wrap",
+            properties: {
+              // passed as a prop to OmniLink
+              isHeading: true,
+            },
+          },
+        ],
+      ],
+    }),
+    sitemap(),
+    react(),
+  ],
 });
