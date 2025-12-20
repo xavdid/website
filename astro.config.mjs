6f9e9d016de20494fcbf7f06d0874fdfc0edab41
David Brownman <oss@xavd.id>
2025-12-19 23:49:53 -0800

    add nice codeblocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,32 +2,42 @@
 
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import expressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://xavd.id",
-  integrations: [
-    mdx({
-      rehypePlugins: [
-        // the IDs get generated without this, but the autolinking doesn't work?
-        rehypeHeadingIds,
-        [
-          rehypeAutolinkHeadings,
-          {
-            behavior: "wrap",
-            properties: {
-              // passed as a prop to OmniLink
-              isHeading: true,
-            },
+  // have to configure these here, since expressiveCode gets wiped out if `mdx()` is configured
+  // see: https://github.com/expressive-code/expressive-code/issues/330
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            // passed as a prop to OmniLink
+            isHeading: true,
           },
-        ],
+        },
       ],
+    ],
+  },
+  integrations: [
+    expressiveCode({
+      frames: false,
+      styleOverrides: {
+        borderColor: "#00eaff",
+        borderWidth: "1px",
+        codeLineHeight: "1.4",
+      },
     }),
+    mdx(),
     sitemap(),
     react(),
   ],
