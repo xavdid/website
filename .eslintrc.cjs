376e3bb6e4a9109961fb184a70de142341e6f569
David Brownman <oss@xavd.id>
2026-01-11 03:50:07 -0800

    fix linter breaking mdx lists

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,8 @@ module.exports = {
       files: ["*.mdx"],
       rules: {
         "react/jsx-indent": 0,
+        // messes with ul lists that take markdown content
+        "react/self-closing-comp": 0,
       },
       globals: {
         YoutubeEmbed: true,
